7398d8ad60145fc5e593aa334b55b8801642609b
Chen <zdms1987@hotmail.com>
2019-03-18 13:35:32 -0500

    add package file

---
+++ hexomap/__init__.py
@@ -0,0 +1,6 @@
+"""
+HEXOMAP is a Cuda-based (realized through pycuda) near-filed
+high-energy X-ray diffraction (NF-HEDM) reconstruction
+toolkit that provides 3D microstructure reconstructeds
+with high fadelity and efficiency.
+"""
