168ec0245df60f8b57ed3b8a0476a8c416050917
Chen <zdms1987@hotmail.com>
2019-03-18 16:03:04 -0500

    start working on the new orientation class

--- hexomap/orientation.py
+++ hexomap/orientation.py
@@ -5,6 +5,54 @@
 General math module for crystal orientation related calculation.
 """
 
+import numpy as np
+from dataclasses import dataclass
+
+
+@dataclass
+class Eulers:
+    """Euler angles representation of orientation."""
+    phi1: float
+    phi:  float
+    phi2: float
+    in_radians: bool=True
+    order: str='zxz'
+
+    @property
+    def as_array(self):
+        return np.array([self.phi1, self.phi, self.phi2])
+
+
+@dataclass
+class Quaternion:
+    """
+    Quaternion representation of orientation.
+            q = w + Xi + Yj + Zk
+    
+    reference:
+        http://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/
+    """
+    w: float  # cos(theta/2)
+    x: float  # sin(theta/2) * rotation_axis_x
+    y: float  # sin(theta/2) * rotation_axis_y
+    z: float  # sin(theta/2) * rotation_axis_z
+    normalized: bool=True
+
+    def __post__init__(self):
+        # quaternion need to be a unit vector to represent orientation
+        if not self.normalized:
+            self.normalize()
+
+    @property
+    def magnitude(self):
+        return np.sqrt(self.w**2 + self.x**2 + self.y**2 + self.z**2)
+
+    def normalize(self):
+        self.w /= self.magnitude
+        self.x /= self.magnitude
+        self.y /= self.magnitude
+        self.z /= self.magnitude
 
 if __name__ == "__main__":
-    pass
+    q = Quaternion(1, 0, 0, 0)
+    print(q)
