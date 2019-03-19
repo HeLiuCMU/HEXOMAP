552f2ee7f84b8fd9f68b6ec15fd2ab6d543b5b22
Chen <zdms1987@hotmail.com>
2019-03-19 11:23:35 -0500

    add place holder for npmath module

---
+++ hexomap/npmath.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 no BOM -*-
+
+"""
+Useful math functions for processing HEDM data implemented in numpy.
+"""
+
+import numpy as np
+
+
+if __name__ == "__main__":
+    pass
---
+++ hexomap/tests/test_npmath.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 no BOM -*-
+
+"""
+Unit test for the npmath module from hexomap package.
+"""
+
+import unittest
+import numpy as np
+
+
+if __name__ == "__main__":
+    pass
