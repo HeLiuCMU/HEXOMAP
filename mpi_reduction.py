3f5c1ff08d859152ddbc8a9adb403f6700c9242c
heliucmu <liuhe1118@gmail.com>
2019-03-18 21:39:37 -0400

    optimize reduction

--- mpi_reduction.py
+++ mpi_reduction.py
@@ -8,7 +8,7 @@ atexit.register(MPI.Finalize)
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
-
+from reduction import segmentation_numba
 # reduce a layer of images together:
 import IntBin
 import time
@@ -23,8 +23,9 @@ end = '.tif'
 initial = '/home/heliu/work/shahani_feb19_part/nf_part/dummy_2_rt_before_heat_nf/dummy_2_rt_before_heat_nf_'
 digitLength = 6
 bkgInitial = 'test_output_bkg'
-binInitial = 'mpi_output/dummy_2_rt_before_heat_nf_test'
-generateBkg = False
+binInitial = '/home/heliu/work/dev/reduction/mpi_output_test/dummy_2_rt_before_heat_nf_test'
+generateBkg = True
+generateBin = False
 baseline = 10
 minNPixel = 4
 ####################################################################################
@@ -43,7 +44,9 @@ lIdxDet = lIdxDet[rank*NPerCore:(rank+1)*NPerCore]
 lIdxRot = lIdxRot[rank*NPerCore:(rank+1)*NPerCore]
 lBkgIdx = lBkgIdx[rank*NPerCore:(rank+1)*NPerCore]
 # generate background:
-print('start generating bkg')
+if rank==0:
+    print('start generating bkg')
+    start =  time.time()
 if generateBkg:
     if rank==0:
         lBkg = reduction.median_background(initial, startIdx, bkgInitial,NRot=NRot, NDet=NDet, NLayer=NLayer,end=end)
@@ -57,21 +60,24 @@ else:
         for det in range(NDet):
             lBkg.append(np.load(f'{bkgInitial}_z{layer}_det_{det}.npy'))
 comm.Barrier()
-print('end generating bkg')
-
 if rank==0:
-    start = time.time()
-    
-for i in range(NPerCore):
-    bkg = lBkg[lBkgIdx[i]]
-    fName = f'{initial}{str(lIdxImg[i]).zfill(digitLength)}{end}'
-    img = plt.imread(fName)
-    binFileName = f'{binInitial}z{idxLayer[lIdxLayer[i]]}_{str(lIdxRot[i]).zfill(digitLength)}.bin{lIdxDet[i]}'
-    print(binFileName)
-    snp = reduction.segmentation(img, bkg, baseline=baseline, minNPixel=minNPixel)
-    IntBin.WritePeakBinaryFile(snp, binFileName) 
-comm.Barrier()
-
-if rank==0:
+    print('end generating bkg')
     end = time.time()
-    print(f'time take: {end - start} seconds')
+    print(f'time take generating bkg: {end-start}')
+if generateBin:
+	if rank==0:
+	    start = time.time()
+	    
+	for i in range(NPerCore):
+	    bkg = lBkg[lBkgIdx[i]]
+	    fName = f'{initial}{str(lIdxImg[i]).zfill(digitLength)}{end}'
+	    img = plt.imread(fName)
+	    binFileName = f'{binInitial}z{idxLayer[lIdxLayer[i]]}_{str(lIdxRot[i]).zfill(digitLength)}.bin{lIdxDet[i]}'
+	    #print(binFileName)
+	    snp = segmentation_numba(img, bkg, baseline=baseline, minNPixel=minNPixel)
+	    IntBin.WritePeakBinaryFile(snp, binFileName) 
+	comm.Barrier()
+
+	if rank==0:
+	    end = time.time()
+	    print(f'time taken generating binary: {end - start} seconds')
