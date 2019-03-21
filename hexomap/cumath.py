e2b2120f4bd3787631b68d3cf424511f06cc2e66
Chen <zdms1987@hotmail.com>
2019-03-21 15:32:46 -0500

    migrate RandomCuda.py to cumath.py

---
+++ hexomap/cumath.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""
+Auxilary module for math functions realized through Cuda.
+"""
+import os
+import hexomap
+import numpy           as     np
+from   hexomap.utility import load_kernel_code
+from   pycuda.compiler import SourceModule
+import pycuda.gpuarray as     gpuarray
+
+
+def curandom(N: int, nvalues: int):
+    """
+    Description
+    -----------
+
+    Parameters
+    ----------
+    N: int
+
+    nvalues: int
+
+    Returns
+    -------
+
+    """
+    # load&compile GPU code
+    _kernel_code = os.path.join(os.path.dirname(hexomap.__file__), 
+                                "kernel_cuda/random_cuda.cu",
+                            )
+    cuda_kernel = SourceModule(load_kernel_code(_kernel_code).replace('%(NGENERATORS)', N))
+
+    # get device code
+    init_func = mod.get_function("_Z10initkerneli")
+    fill_func = mod.get_function("_Z14randfillkernelPfi")
+
+    # get the random numbers
+    init_func(seed, block=(N,1,1), grid=(1,1,1))
+    gdata = gpuarray.zeros(nvalues, dtype=np.float32)
+    fill_func(gdata, np.int32(nvalues), block=(N,1,1), grid=(1,1,1))
+
+    return gdata
+
+if __name__ == "__main__":
+    pass
