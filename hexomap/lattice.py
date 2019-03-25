7dcbe85207f0643238f373f46d32104af198e622
Chen <zdms1987@hotmail.com>
2019-03-25 16:56:37 -0500

    the ipf color now should work

--- hexomap/lattice.py
+++ hexomap/lattice.py
@@ -21,6 +21,7 @@ import numpy as np
 from dataclasses         import dataclass
 from hexomap.orientation import Quaternion
 from hexomap.orientation import Orientation
+from hexomap.npmath      import normalize
 
 
 def sym_operator(lattice:str) -> list:
@@ -28,7 +29,8 @@ def sym_operator(lattice:str) -> list:
     Description
     -----------
     Return a list of symmetry operator in quaternions based on given lattice
-    structure.
+    structure.  These quaternion are meant to operator on vectors in the
+    crystal frame.
 
     Parameters
     ----------
@@ -39,9 +41,15 @@ def sym_operator(lattice:str) -> list:
     -------
     list
         list of quaternions as symmetry operators
+
+    NOTE
+    ----
+    This function only provides a list, which is not associated with frame.
+    Therefore, one need to keep in mind that these operator are meant for
+    vectors in crystal frame.
     """
     if lattice is None:
-        return Quaternion(1,0,0,0)
+        return [Quaternion(1,0,0,0)]
     elif lattice.lower() in ['orthorhombic', 'ortho']:
         return [
             Quaternion(*me) for me in [
@@ -115,35 +123,6 @@ def sym_operator(lattice:str) -> list:
         ]
     else:
         raise ValueError(f"Unknown lattice structure {lattice}")
-
-
-def vec_in_standard_stereographic_triangle(vec: np.ndarray, 
-                                           lattice: str,
-                                        ) -> bool:
-    """
-    Description
-    -----------
-    Check whether a given vector will project to the standard stereographic
-    triangle, useful for computing Inverse Pole Figure color tuple.
-        ---------
-        !Warning!
-        ---------
-        The calculation performed here is assuming that the vector is in
-        the lattice framework.
-    
-    Parameters
-    ----------
-    vec: np.ndarray
-        target vector
-    lattice: str
-        lattice name
-    
-    Returns
-    -------
-    bool
-    """
-    # Considers only vectors with z >= 0, hence uses two neighboring SSTs.
-    vec = vec * np.sign(vec[2])
 
 
 def in_fundamental_zone(o: "Orientation", lattice: str) -> bool:
@@ -244,6 +223,141 @@ def in_standard_stereographic_triangle(o: 'Orientation',
             and r[2] >= 0
     else:
         return True
+
+
+def calc_inverse_pole_figure_color(o: 'Orientation',
+                                   p: np.ndarray,
+                                   lattice: str,
+                                ) -> tuple:
+    """
+    Description
+    -----------
+        Calculate the Inverse Pole Figure (IPF) color tuple common used to
+        represent the crystal orientations in an EBSD map.
+        The orientation and the pole must be associated with the same reference
+        frame.
+
+        !WARNING!
+            This function does not provide frame check.  It is the users
+            responsibility to ensure that the orientation and the pole are
+            associated with the same frame.
+
+    Parameter
+    ---------
+    o: Orientation
+        Crystal orientation, preferably associated with the sample frame
+    p: np.ndarray
+        Pole direction, often expressed in the sample frame, such as
+            ND: sample normal directon
+            RD: sample rolling direction
+            TD: transverse direction (or tensile direction)
+    lattice: str
+        Lattice name
+    
+    Returns
+    -------
+    tuple
+        RGB color tuple with range between 0 and 1 
+    """
+    # convert the pole from the common (sample) frame to the crystal frame
+    rot_m = o.as_matrix     # active rotation matrix
+    p = np.dot(rot_m.T, p)  # bring pole to the crystal frame
+    p = normalize(p)
+
+    # generate the three reference pole (crystal frame) in the IPF triangle
+    basis = get_inverse_pole_figure_ref_poles(lattice)
+
+    # use the coefficient as the RGB IPF color tuple
+    #              basis                ipf          p
+    #              -----                ---          -
+    #     | red_1  green_1  blue_1 | | ipf_r |  = | p_1 |
+    #     | red_2  green_2  blue_2 | | ipf_g |  = | p_2 |
+    #     | red_3  green_3  blue_3 | | ipf_b |  = | p_3 |
+    #
+    for sym_opt in sym_operator(lattice):
+        p = Quaternion.quatrotate(sym_opt, p)
+        ipf = np.dot(np.linalg.inv(basis), p)
+        if np.all(ipf < 0):
+            continue  # not in the SST, check next equivalent orientation
+        else:
+            ipf = np.minimum(normalize(ipf)**0.5, np.ones(3))
+            return ipf/ipf.max()
+    # if we cannot find any, something is seriously wrong
+    raise ValueError("Cannot find equivalent orientation in SST")
+
+
+def get_inverse_pole_figure_ref_poles(lattice: str):
+    """
+    Description
+    -----------
+        Return the reference poles (crystal frame) for given lattice
+    
+    Parameters
+    ----------
+    lattice: str
+        lattice name
+    
+    Returns
+    -------
+    np.ndarray
+        basis in crystal frame
+    """
+    if lattice.lower() in ['cubic', 'bcc', 'fcc']:
+        basis = np.array([
+            [0, 0, 1],   # red pole
+            [1, 0, 1],   # green pole
+            [1, 1, 1],   # blue pole
+        ])
+    elif lattice.lower() in ['hexagonal', 'hcp', 'hex']:
+        basis = np.array([
+            [0,              0, 1],  # c-axis, [0001], red
+            [1,              0, 0],  # a-axis, [2-1-10], green
+            [np.sqrt(3)/2, 0.5, 0],  # [10-10], blue
+        ])
+    elif lattice.lower() in ['tetragonal', 'tet']:
+        basis = np.array([
+            [0, 0, 1],  # red
+            [1, 0, 0],  # green
+            [1, 1 ,0],  # blue
+        ])
+    elif lattice.lower() in ['orthorhombic', 'ortho']:
+        basis = np.array([
+            [0, 0, 1],  # red
+            [1, 0, 0],  # green
+            [0, 1, 0],  # blue
+        ])
+    else:
+        raise ValueError(f"Unknown lattice {lattice}")
+    return normalize(basis.T, axis=0)
+
+
+def basis_lattice(lattice: str) -> np.ndarray:
+    """
+    Description
+    -----------
+        Return a matrix whose column space is the three lattice vector
+            m = | a_1  b_1  c_1 |
+                | a_2  b_2  c_2 |
+                | a_3  b_3  c_3 |
+        the covariances (a|b|c_i) are in the crystal frame
+
+    Parameters
+    ----------
+    lattice: str
+        lattice name
+
+    Returns
+    -------
+    np.ndarray
+        column space matrix
+    """
+    pass
+
+
+def reciprocal_basis_lattice(lattice: str) -> np.ndarray:
+    """
+    """
+    pass
 
 
 if __name__ == "__main__":
