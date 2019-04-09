a390da713a31eb8c34966e68f57cd54352b42e68
Chen <zdms1987@hotmail.com>
2019-04-09 12:21:24 -0500

    migrate useful io function to utility

--- hexomap/utility.py
+++ hexomap/utility.py
@@ -4,6 +4,8 @@
 Utility module for hexomap package.
 """
 
+import yaml
+import h5py
 import numpy as np
 
 from functools import singledispatch
@@ -53,5 +55,75 @@ def standarize_euler(euler: np.ndarray, in_radian=True) -> np.ndarray:
         )
 
 
+def load_yaml(fname: str) -> dict:
+    """generic safe_load wrapper for yaml archive"""
+    try:
+        with open(fname, 'r') as f:
+            dataMap = yaml.safe_load(f)
+    except IOError as e:
+        print(f"Cannot open YAML file {fname}")
+        print(f"IOError: {e}")
+    
+    return dataMap
+
+
+def write_yaml(fname: str, data: dict) -> None:
+    """generic output handler for yaml archive"""
+    try:
+        with open(fname, 'w') as f:
+            yaml.safe_dump(data, f, default_flow_style=False)
+    except IOError as e:
+        print(f"Cannot write YAML file {fname}")
+        print(f"IOError: {e}")
+
+
+def write_h5(fname: str, data: dict) -> None:
+    """generic simper HDF5 archive writer"""
+    try:
+        with h5py.File(fname, 'w') as f:
+            recursively_save_dict_contents_to_group(f,'/',data)
+    except IOError as e:
+        print(f"Cannot write HDF5 file {fname}")
+        print(f"IOError: {e}")
+
+
+def load_h5(fname: str, path: str='/') -> dict:
+    """generic simple HDF5 archive loader"""
+    try:
+        with h5py.File(fname, 'r') as f:
+            dataMap = recursively_load_dict_contents_from_group(f, path)
+    except IOError as e:
+        print(f"Cannot open HDF5 file {fname}")
+        print(f"IOError: {e}")
+
+    return dataMap
+
+
+def recursively_load_dict_contents_from_group(h5file: "h5py.File", 
+                                              path: str,
+        ) -> dict:
+    """recursively load data from HDF5 archive as dict"""
+    ans = {}
+    for key, item in h5file[path].items():
+        if isinstance(item, h5py._hl.dataset.Dataset):
+            ans[key] = item.value
+        elif isinstance(item, h5py._hl.group.Group):
+            ans[key] = recursively_load_dict_contents_from_group(h5file, f"{path}{key}/")
+    return ans
+
+
+def recursively_save_dict_contents_to_group(h5file: "h5py.File", 
+                                            path: str, 
+                                            dic: dict,
+        ) -> None:
+    """recursively write data to HDF5 archive"""
+    for key, item in dic.items():
+        if isinstance(item, (np.ndarray, np.int64, np.float64, str, bytes,int,float,np.bool_)):
+            h5file[path + key] = item
+        elif isinstance(item, dict):
+            recursively_save_dict_contents_to_group(h5file, path + key + '/', item)
+        else:
+            raise ValueError(f'Cannot save {item} type')
+            
 if __name__ == "__main__":
     pass
