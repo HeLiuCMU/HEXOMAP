998b84694dc1c16c59e3a5a205b199e07cc2dc49
heliucmu <liuhe1118@gmail.com>
2019-05-28 12:31:55 -0400

    setup.py add non-python files

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='hexomap',
       version='0.2',
+      package_dir={'': ''},
       packages=['hexomap'],
+      package_data={'hexomap': ['data/fundamental_zone/*','data/materials/*','kernel_cuda/*']},
       )
